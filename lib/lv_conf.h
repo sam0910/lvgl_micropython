d6b21e44a10794126f45eb4fae8fc3351752b480
sam0910 <divekr@gmail.com>
2024-08-27 05:12:46 +0900

    [0827/05:12] custom font declare

--- lib/lv_conf.h
+++ lib/lv_conf.h
@@ -530,7 +530,7 @@ extern void *mp_lv_roots;
 /*Optionally declare custom fonts here.
  *You can use these fonts as default font too and they will be available globally.
  *E.g. #define LV_FONT_CUSTOM_DECLARE   LV_FONT_DECLARE(my_font_1) LV_FONT_DECLARE(my_font_2)*/
-#define LV_FONT_CUSTOM_DECLARE
+#define LV_FONT_CUSTOM_DECLARE LV_FONT_DECLARE(font_wifi_16) LV_FONT_DECLARE(font_wifi_22)
 
 /*Always set a default font*/
 #define LV_FONT_DEFAULT &lv_font_montserrat_14
